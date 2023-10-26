866e49fe484d3d3d67d98ec76d3c7b245ab49ecf
tamhv <tamhv@battech.vn>
2023-10-26 08:58:03 +0700

    [IMP] CartPage: fix theme cart page

--- src/pages/CartPage/CartPage.js
+++ src/pages/CartPage/CartPage.js
@@ -1,109 +1,111 @@
-import React from "react";
-import "./CartPage.scss";
-import { useSelector, useDispatch } from "react-redux";
-import { shopping_cart } from "../../utils/images";
-import { Link } from "react-router-dom";
+import React from 'react'
+import './CartPage.scss'
+import { useSelector, useDispatch } from 'react-redux'
+import { shopping_cart } from '../../utils/images'
+import { Link } from 'react-router-dom'
 import {
   getAllCarts,
   removeFromCart,
-  toggleCartQty,
-} from "../../store/cartSlice";
-import { Divider } from "antd";
+  toggleCartQty
+} from '../../store/cartSlice'
+import { Divider } from 'antd'
 import Checkout from './checkout'
 
+import Button from '@mui/material/Button'
+
 const CartPage = () => {
-  const dispatch = useDispatch();
-  const carts = useSelector(getAllCarts);
-  const { itemsCount, totalAmount } = useSelector((state) => state.cart);
+  const dispatch = useDispatch()
+  const carts = useSelector(getAllCarts)
+  const { itemsCount, totalAmount } = useSelector(state => state.cart)
 
   if (carts.length === 0) {
     return (
-      <div className="container my-5">
-        <div className="empty-cart flex justify-center align-center flex-column font-manrope">
-          <img src={shopping_cart} alt="" />
-          <span className="fw-6 fs-15 text-gray">Giỏ hàng đang trống.</span>
-          <Link to="/" className="shopping-btn bg-orange text-white fw-5">
+      <div className='container my-5'>
+        <div className='empty-cart flex justify-center align-center flex-column font-manrope'>
+          <img src={shopping_cart} alt='' />
+          <span className='fw-6 fs-15 text-gray'>Giỏ hàng đang trống.</span>
+          <Link to='/' className='shopping-btn bg-orange text-white fw-5'>
             Đi tới mua hàng
           </Link>
         </div>
       </div>
-    );
+    )
   }
 
   return (
     <>
-      <h3 className="text-center fw-5" style={{ marginTop: 20 }}>
+      <h3 className='text-center fw-5' style={{ marginTop: 20 }}>
         Giỏ hàng của bạn
       </h3>
       <Divider />
       <div
-        className="cart bg-white"
-        style={{ margin: `20px auto`, width: `50%`, borderRadius: "20px" }}
+        className='cart bg-white'
+        style={{ margin: `20px auto`, width: `50%`, borderRadius: '20px' }}
       >
-        <div className="container">
-          <div className="cart-ctable">
-            <div className="cart-cbody bg-white">
+        <div className='container'>
+          <div className='cart-ctable'>
+            <div className='cart-cbody bg-white'>
               {carts.map((cart, idx) => {
                 return (
                   <>
                     <div
-                      className="cart-ctr"
+                      className='cart-ctr'
                       key={cart?.id}
                       style={{ marginTop: 10 }}
                     >
-                      <div className="cart-ctd">
+                      <div className='cart-ctd'>
                         <img
                           style={{ width: 112, height: 105 }}
-                          src="https://cdn.tgdd.vn/Products/Images/42/235838/Galaxy-S22-Ultra-Black-200x200.jpg"
+                          src='https://cdn.tgdd.vn/Products/Images/42/235838/Galaxy-S22-Ultra-Black-200x200.jpg'
                         />
 
                         <button
-                          type="button"
-                          className="delete-btn text-dark"
+                          type='button'
+                          className='delete-btn text-dark'
                           onClick={() => dispatch(removeFromCart(cart?.id))}
                           style={{
-                            color: "#999",
-                            position: "relative",
-                            right: "-42px",
-                            marginTop: "7px",
+                            color: '#999',
+                            position: 'relative',
+                            right: '-42px',
+                            marginTop: '7px'
                           }}
                         >
-                          <i class="fa-regular fa-circle-xmark"></i> Xóa
+                          <i class='fa-regular fa-circle-xmark'></i> Xóa
                         </button>
                       </div>
                       <div
-                        className="cart-ctd"
-                        style={{ position: "relative", top: "0px" }}
+                        className='cart-ctd'
+                        style={{ position: 'relative', top: '0px' }}
                       >
                         <div
                           style={{
-                            display: "flex",
-                            justifyContent: "space-between",
+                            display: 'flex',
+                            justifyContent: 'space-between',
                             width: 550,
                             height: 140,
-                            marginLeft: 10,
+                            marginLeft: 10
                           }}
                         >
-                          <div style={{ width: "127%" }}>
-                            <span className="cart-ctxtf fw-7">
+                          <div style={{ width: '127%' }}>
+                            <span className='cart-ctxtf fw-7'>
                               Laptop Asus TUF Gaming F15 FX506HE i7
                             </span>
                             <br />
                             <span>Màu : đen</span>
                           </div>
-                          <div style={{ width: "58%" }}>
+                          <div style={{ width: '58%' }}>
                             <span
-                              className="cart-ctxt"
-                              style={{ color: "red", fontSize: "16px" }}
+                              className='cart-ctxt'
+                              style={{ color: 'red', fontSize: '16px' }}
                             >
-                              9.0000.000{" "}
+                              9.0000.000{' '}
                               <sup>
                                 <ins>đ</ins>
                               </sup>
                             </span>
                             <br />
-                            <del style={{ color: "#999", fontSize: "16px" }}>
-                              9.5000.000{" "}
+                            <del style={{ color: '#999', fontSize: '16px' }}>
+                              9.5000.000{' '}
                               <sup>
                                 <ins>đ</ins>
                               </sup>
@@ -111,47 +113,47 @@ const CartPage = () => {
                           </div>
                         </div>
                       </div>
-                      <div className="cart-ctd"></div>
-                      <div className="cart-ctd">
+                      <div className='cart-ctd'></div>
+                      <div className='cart-ctd'>
                         <div
-                          className="qty-change flex align-center"
+                          className='qty-change flex align-center'
                           style={{
                             position: `relative`,
                             top: `39px`,
-                            right: `171px`,
+                            right: `171px`
                           }}
                         >
                           <button
-                            type="button"
-                            className="qty-decrease flex align-center justify-center"
+                            type='button'
+                            className='qty-decrease flex align-center justify-center'
                             onClick={() =>
                               dispatch(
-                                toggleCartQty({ id: cart?.id, type: "DEC" })
+                                toggleCartQty({ id: cart?.id, type: 'DEC' })
                               )
                             }
                           >
-                            <i className="fas fa-minus"></i>
+                            <i className='fas fa-minus'></i>
                           </button>
 
-                          <div className="qty-value flex align-center justify-center">
+                          <div className='qty-value flex align-center justify-center'>
                             {cart?.quantity}
                           </div>
 
                           <button
-                            type="button"
-                            className="qty-increase flex align-center justify-center"
+                            type='button'
+                            className='qty-increase flex align-center justify-center'
                             onClick={() =>
                               dispatch(
-                                toggleCartQty({ id: cart?.id, type: "INC" })
+                                toggleCartQty({ id: cart?.id, type: 'INC' })
                               )
                             }
                           >
-                            <i className="fas fa-plus"></i>
+                            <i className='fas fa-plus'></i>
                           </button>
                         </div>
                       </div>
 
-                      <div className="cart-ctd">
+                      <div className='cart-ctd'>
                         {/* <span
                           className="cart-ctxt text-orange fw-5"
                           style={{ color: "#128DE2" }}
@@ -160,83 +162,63 @@ const CartPage = () => {
                         </span> */}
                       </div>
 
-                      <div className="cart-ctd"></div>
+                      <div className='cart-ctd'></div>
                     </div>
                   </>
-                );
+                )
               })}
             </div>
 
             <div
               style={{
-                display: "flex",
+                display: 'flex',
                 justifyContent: `space-between`,
-                width: "90%",
-                margin: "0px auto",
-                transform: `translateY(24px)`,
+                width: '90%',
+                margin: '0px auto',
+                transform: `translateY(24px)`
               }}
             >
               <div>Tạm tính ({itemsCount} sản phẩm) :</div>
               <div>
-                {totalAmount}{" "}
+                {totalAmount}{' '}
                 <sup>
                   <ins>đ</ins>
                 </sup>
               </div>
             </div>
+
             <Divider />
-            {/* 
-            <div className="cart-cfoot flex align-start justify-between py-3 bg-white">
-              <div className="cart-cfoot-l">
-                <button
-                  type="button"
-                  style={{
-                    border: `1px solid #128DE2`,
-                    color: `#128DE2`,
-                    borderRadius: `14px`,
-                    fontSize: `12px`,
-                  }}
-                  className="clear-cart-btn text-danger fs-15 text-uppercase fw-4"
-                  onClick={() => dispatch(clearCart())}
-                >
-                  <i className="fas fa-trash"></i>
-                  <span className="mx-1">Xóa toàn bộ sản phẩm</span>
-                </button>
+
+            <Checkout />
+
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: `space-between`,
+                width: '90%',
+                margin: '0px auto',
+                marginTop: '-30px'
+              }}
+            >
+              <div className='fw-6'>Tổng tiền :</div>
+              <div style={{ color: 'red' }}>
+                {totalAmount}{' '}
+                <sup>
+                  <ins>đ</ins>
+                </sup>
               </div>
+            </div>
 
-              <div className="cart-cfoot-r flex flex-column justify-end">
-                <div className="total-txt flex align-center justify-end">
-                  <div className="font-manrope fw-5">
-                    Tổng ({itemsCount}) sản phẩm:{" "}
-                  </div>
-                  <span
-                    className="text-orange fs-22 mx-2 fw-6"
-                    style={{ color: `#128DE2` }}
-                  >
-                    {formatPrice(totalAmount)}
-                  </span>
-                </div>
+            <div>
+            <Button variant='contained' style={{ width: '90%', marginTop:22, marginLeft:'35px', fontSize: 16 }}>Đặt hàng</Button>
+            </div>
 
-                <button
-                  style={{
-                    backgroundImage: `linear-gradient(0deg, rgb(0, 102, 204), rgb(0, 153, 204))`,
-                    borderRadius: `14px`,
-                  }}
-                  type="button"
-                  className="checkout-btn text-white bg-orange fs-16"
-                >
-                  Thanh toán
-                </button>
-              </div>
-            </div> */}
-
-             <Checkout/>
-            <br/>
+            <br />
           </div>
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default CartPage;
+export default CartPage
--- src/pages/CartPage/checkout.js
+++ src/pages/CartPage/checkout.js
@@ -1,73 +1,86 @@
-import React, {useState} from "react";
-import "./CartPage.scss";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import TextField from "@mui/material/TextField";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-
+import React, { useState } from 'react'
+import './CartPage.scss'
+import Radio from '@mui/material/Radio'
+import RadioGroup from '@mui/material/RadioGroup'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import TextField from '@mui/material/TextField'
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormHelperText from '@mui/material/FormHelperText'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import FormGroup from '@mui/material/FormGroup'
+import Checkbox from '@mui/material/Checkbox'
+import { Divider } from 'antd'
+import Button from '@mui/material/Button'
 
 const CartPage = () => {
+  const [age, setAge] = React.useState('')
+
+  const handleChange = event => {
+    setAge(event.target.value)
+  }
 
   const bull = (
     <Box
-      component="span"
-      sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+      component='span'
+      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
     >
       •
     </Box>
-  );
+  )
 
   return (
     <>
-      <div style={{ width: "90%", margin: "0px auto" }}>
+      <div style={{ width: '90%', margin: '0px auto' }}>
         <div>THÔNG TIN KHÁCH HÀNG</div>
 
         <div>
           <RadioGroup
             row
-            aria-labelledby="demo-row-radio-buttons-group-label"
-            name="row-radio-buttons-group"
+            aria-labelledby='demo-row-radio-buttons-group-label'
+            name='row-radio-buttons-group'
           >
-            <FormControlLabel value="male" control={<Radio />} label="Anh" />
-            <FormControlLabel value="female" control={<Radio />} label="Chị" />
+            <FormControlLabel value='male' control={<Radio />} label='Anh' />
+            <FormControlLabel value='female' control={<Radio />} label='Chị' />
           </RadioGroup>
         </div>
 
         <div>
           <TextField
-            id="outlined-basic"
-            label="Họ và tên"
-            variant="outlined"
+            id='outlined-basic'
+            label='Họ và tên'
+            variant='outlined'
             style={{ width: 230 }}
           />
           <TextField
-            id="outlined-basic"
-            label="Số điện thoại"
-            variant="outlined"
+            id='outlined-basic'
+            label='Số điện thoại'
+            variant='outlined'
             style={{ width: 230, marginLeft: 20, fontSize: 16 }}
           />
         </div>
 
-        <div style={{ margin: "10px 0" }}>CHỌN CÁCH THỨC NHẬN HÀNG</div>
+        <div style={{ margin: '10px 0' }}>CHỌN CÁCH THỨC NHẬN HÀNG</div>
         <div>
           <RadioGroup
             row
-            aria-labelledby="demo-row-radio-buttons-group-label"
-            name="row-radio-buttons-group"
+            aria-labelledby='demo-row-radio-buttons-group-label'
+            name='row-radio-buttons-group'
           >
             <FormControlLabel
-              value="male"
+              value='male'
               control={<Radio />}
-              label="Giao tận nơi"
+              label='Giao tận nơi'
             />
             <FormControlLabel
-              value="female"
+              value='female'
               control={<Radio />}
-              label="Nhận tại cửa hàng"
+              label='Nhận tại cửa hàng'
             />
           </RadioGroup>
         </div>
@@ -75,18 +88,127 @@ const CartPage = () => {
         <div>
           <Card sx={{ minWidth: 275 }}>
             <CardContent>
-              <Typography variant="h5" component="div">
+              <Typography variant='h5' component='div'>
                 Chọn địa chỉ để biết thời gian nhận hàng và phí vận chuyển (nếu
                 có)
               </Typography>
 
+              <FormControl sx={{ m: 1, minWidth: 230 }}>
+                <InputLabel id='select-city'>Chọn thành phố</InputLabel>
+                <Select
+                  labelId='select-city'
+                  id='demo-simple-select-helper'
+                  value={age}
+                  label='Age'
+                  onChange={handleChange}
+                >
+                  <MenuItem value=''>
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+
+              <FormControl sx={{ m: 1, minWidth: 230 }}>
+                <InputLabel id='select-city'>Chọn Quận/Huyện</InputLabel>
+                <Select
+                  labelId='select-city'
+                  id='demo-simple-select-helper'
+                  value={age}
+                  label='Age'
+                  onChange={handleChange}
+                >
+                  <MenuItem value=''>
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+
+              <FormControl sx={{ m: 1, minWidth: 230 }}>
+                <InputLabel id='select-city'>Chọn Phường/Xã</InputLabel>
+                <Select
+                  labelId='select-city'
+                  id='demo-simple-select-helper'
+                  value={age}
+                  label='Age'
+                  onChange={handleChange}
+                >
+                  <MenuItem value=''>
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+
+              <TextField
+                id='outlined-basic'
+                label='Số nhà/Tên đường'
+                variant='outlined'
+                style={{
+                  width: 230,
+                  marginLeft: 10,
+                  marginTop: 7,
+                  minHeight: `1.4375em`,
+                  fontSize: 16
+                }}
+              />
             </CardContent>
           </Card>
+        </div>
+
+        <div>
+          <TextField
+            id='outlined-basic'
+            label='Yêu cầu khác không bắt buộc'
+            variant='outlined'
+            style={{ width: '100%', margin: `20px 0px`, fontSize: 16 }}
+          />
+        </div>
+
+        <div>
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label='Gọi người khác nhận hàng (nếu có)'
+            />
+            <FormControlLabel
+              required
+              control={<Checkbox />}
+              label='Hướng dẫn sử dụng, giải đáp thắc mắc sản phẩm'
+            />
+            <FormControlLabel
+              required
+              control={<Checkbox />}
+              label='Xuất hóa đơn công ty'
+            />
+          </FormGroup>
+
+          <Divider />
+
+          <div style={{ marginTop:'-20px',marginBottom:'-20px'}}>
+            <TextField
+              id='outlined-basic'
+              placeholder='Nhập mã giảm giá hoặc phiếu mua hàng'
+              variant='outlined'
+              style={{ width: '60%', margin: `20px 0px`, fontSize: 16 }}
+            />
+            <Button variant='contained' style={{ width: '20%', marginLeft:40, marginTop:22, fontSize: 16 }}>Áp dụng</Button>
+          </div>
+
+          <Divider />
+
         </div>
       </div>
       <br />
     </>
-  );
-};
+  )
+}
 
-export default CartPage;
+export default CartPage
