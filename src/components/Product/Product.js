a77a57554f1391071b29c2d424567eabb68ea2fa
tamhv <pohoang95@gmail.com>
2023-10-26 19:56:45 +0700

    [FIX] product: fix products

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,27 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import {formatPrice} from "../../utils/helpers";
+import React from "react";
+import { Link } from "react-router-dom";
+import { formatPrice } from "../../utils/helpers";
 import "./Product.scss";
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
   return (
-    <Link to = {`/product/${product?.id}`} key = {product?.id}>
-      <div className='product-item bg-white'>
-        <div className='category' style={{ backgroundColor: `#128DE2`}}>Giảm 40%</div>
-        <div className='product-item-img'>
-          <img className='img-cover' src = {product?.images[0]} alt = {product.title} />
+    <Link to={`/product/${product?.id}`} key={product?.id}>
+      <div className="product-item bg-white"
+            style={{
+              width:'250px'
+            }}
+      >
+        <div
+          className="category"
+          style={{
+            backgroundColor: `#128DE2`,
+            position: "relative",
+            width: "30%",
+            top: "25px",
+            borderTopLeftRadius: `8px`,
+            borderTopRightRadius: `20px`,
+            borderBottomRightRadius: `20px`,
+            fontWeight:'600'
+          }}
+        >
+          Giảm 40%
         </div>
-        <div className='product-item-info fs-14'>
-          <div className='brand'>
-            <span className='fw-7' style={{ fontSize: '18px'}}>{product?.title}</span>
+        <div className="product-item-img">
+          <img
+            style={{ width: "86%", margin:'0px auto', }}
+            src='https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-12.png'
+            alt={product.title}
+          />
+        </div>
+        <div className="product-item-info fs-14">
+          <div className="brand">
+            <span className="fw-7" style={{ fontSize: "18px" }}>
+              {product?.title}
+            </span>
           </div>
-          <div className='price flex align-center justify-center'>
-            <span className='old-price'>
-              4.000.000 đ
-            </span>
-            <span className='new-price'>
-              3.000.000 đ
-            </span>
+          <div className="price flex align-center justify-center">
+            <span className="old-price">4.000.000 đ</span>
+            <span className="new-price">3.000.000 đ</span>
             {/* <span className='discount fw-6' style={{ color:`#128DE2` }}>
               (40% Off)
             </span> */}
@@ -29,7 +49,7 @@ const Product = ({product}) => {
         </div>
       </div>
     </Link>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
