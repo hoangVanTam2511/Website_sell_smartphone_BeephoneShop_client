46bdb587e7bb1255f4125dac1ce7d363acef106f
tamhv <pohoang95@gmail.com>
2023-10-27 21:22:44 +0700

    [MOD] Product : add product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -17,7 +17,7 @@ const Product = ({ product }) => {
           style={{
             backgroundColor: `#128DE2`,
             position: "relative",
-            width: "38%",
+            width: "31%",
             top: "1px",
             borderTopLeftRadius: `8px`,
             borderTopRightRadius: `20px`,
@@ -36,13 +36,25 @@ const Product = ({ product }) => {
         </div>
         <div className="product-item-info fs-14">
           <div className="brand">
-            <span className="fw-6" style={{ fontSize: "16px" }}>
+            <span className="fw-6" style={{ fontSize: "14px" }}>
               Iphone 12 6G 128GB
             </span>
           </div>
+
           <div className="price flex align-center justify-center">
-            <span className="new-price" style={{ color:'red' }}>3.000.000 đ</span>
+            <span className="new-price" style={{ color:'rgb(18, 141, 226)' }}>3.000.000 đ</span>
             <span className="old-price">4.000.000 đ</span>
+          </div>
+
+          <div style={{ color:'yellow'}}>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <span style={{ color:'gray'}}>
+              (5000)
+            </span>
           </div>
         </div>
       </div>
--- src/pages/SearchOrderPage/SearchOrderPage.js
+++ src/pages/SearchOrderPage/SearchOrderPage.js
@@ -1,61 +1,59 @@
-import React from 'react'
-import { Tabs, Card } from 'antd'
+import React from "react";
+import { Col, Row } from "antd";
+import Button from "@mui/material/Button";
+import "./SearchOrderPage.css";
+import { Card } from "antd";
+
 const App = () => {
   return (
     <>
-      <div style={{ width: '80%', margin: `20px auto`, display: 'flex' }}>
-        <div style={{ width: '20%' }}>
-          Anh <span className='fw-8'>Hoàng Văn Tám</span>
+      <div style={{ width: "80%", margin: `30px auto`, display: "flex" }}>
+        <div style={{ width: "20%" }}>
+          Anh <span className="fw-8">Hoàng Văn Tám</span>
         </div>
-        <div style={{ width: '70%' }}>
-          <span className='fw-5' style={{ fontSize: 20 }}>
+        <div style={{ width: "70%" }}>
+          <span className="fw-5" style={{ fontSize: 20 }}>
             Đơn hàng đã mua
           </span>
-          <span className='fw-3' style={{ marginLeft: 20, fontSize: 14 }}>
+          <span className="fw-3" style={{ marginLeft: 20, fontSize: 14 }}>
             Từ ngày 26/10/2023 - 26/10/2023
           </span>
         </div>
       </div>
 
-      <div style={{ width: '80%', margin: `20px auto` }}>
-        <Tabs
-          defaultActiveKey='1'
-          tabPosition={`left`}
-          style={{
-            height: 220
-          }}
-          items={new Array(2).fill(null).map((_, i) => {
-            const id = String(i)
-            return {
-              label: `${
-                id === '0' ? 'Đơn hàng đã mua' : 'Thông tin về sổ đia chỉ'
-              }`,
-              key: id,
-              disabled: i === 4,
-              children: `${
-                id === '0' ? 
-                  <>
-                    <Card
-                      title='hehe'
-                      bordered={false}
-                      style={{
-                        width: 300
-                      }}
-                    >
-                      <p>Card content</p>
-                      <p>Card content</p>
-                      <p>Card content</p>
-                    </Card>
-                  </>
-                : (
-                  'Thông tin về sổ đia chỉ'
-                )
-              }`
-            }
-          })}
-        />
+      <div style={{ width: "80%", margin: `20px auto` }}>
+        <Row>
+          <Col span={8}>
+            <div className="order-bought">
+              <i class="fa-solid fa-file-invoice"></i> Đơn hàng đã mua
+            </div>
+
+            <div className="order-bought">
+              <i class="fa-solid fa-id-card"></i> Thông tin và sổ địa chỉ
+            </div>
+            <Button
+              className="button-order-bought"
+              variant="outlined"
+              startIcon={<i class="fa-solid fa-arrow-right-to-bracket"></i>}
+            >
+              Đăng xuất
+            </Button>
+          </Col>
+          <Col span={16}>
+            <Card
+              bordered={false}
+              style={{
+                width: 300,
+              }}
+            >
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card>
+          </Col>
+        </Row>
       </div>
     </>
-  )
-}
-export default App
+  );
+};
+export default App;
