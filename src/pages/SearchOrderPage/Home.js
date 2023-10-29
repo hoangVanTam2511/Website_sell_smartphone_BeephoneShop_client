82c83ebbcd94086e8d525b906e17a210e82e2ffd
tamhv <tamhv@battech.vn>
2023-10-30 06:42:28 +0700

    [FIX] Order : fix order history

---
+++ src/pages/SearchOrderPage/Home.js
@@ -0,0 +1,235 @@
+import React, { useEffect, useState } from 'react'
+import Button from '@mui/material/Button'
+import './SearchOrderPage.css'
+import { Divider } from 'antd'
+import { Select, Space, Input, Checkbox } from 'antd'
+import axios from 'axios'
+
+const Orders = () => {
+  return (
+    <>
+      <div class='card' style={{ width: '100%' }}>
+        <div style={{ width: `25%`, margin: `0px auto` }}>
+          <div
+            style={{
+              width: `70px`,
+              display: `block`,
+              border: `1px solid #707070`,
+              borderRadius: `50%`,
+              padding: `10px`,
+              display: 'block',
+              marginLeft: '32%',
+              height: '70px',
+              backgroundImage: `url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz0LTGso_7ubZRD1CZoDauy3NWC6RxMqVf_A&usqp=CAU)`,
+              backgroundSize: 'cover'
+            }}
+          > </div>
+          <h4 style={{ textAlign: 'center' }}>
+            Xin chào
+            <br />{' '}
+            <span className='fw-7 fs-20' style={{ color: `#128DE2` }}>
+              Hoàng Văn Tám
+            </span>
+          </h4>
+          <h4></h4>
+        </div>
+
+        <div
+          class='title'
+          style={{ textAlign: 'center', justifyContent: 'space-evenly' }}
+        >
+          <h4 style={{ width: '20%' }}>
+            <div className='fw-6' style={{ margin: '10px 0px' }}>
+              Ngày tham gia
+            </div>
+            <div>
+              <i
+                class='fa fa-calendar-check'
+                style={{ fontSize: 50, color: '#128DE2' }}
+              ></i>
+            </div>
+            <div className='fw-6' style={{ margin: '10px 0px' }}>
+              8/1/2023
+            </div>
+          </h4>
+
+          <h4 style={{ width: '20%' }}>
+            <div className='fw-6' style={{ margin: '10px 0px' }}>
+              Hạng thành viên
+            </div>
+            <div>
+              <i
+                class='fa fa-medal'
+                style={{ fontSize: 50, color: '#128DE2' }}
+              ></i>
+            </div>
+            <div className='fw-6' style={{ margin: '10px 0px' }}>
+              Hạng đồng đoàn
+            </div>
+          </h4>
+
+          <h4 style={{ width: '20%' }}>
+            <div className='fw-6' style={{ margin: '10px 0px' }}>
+              Điểm tích luỹ từ 08/01/2023
+            </div>
+            <div>
+              <i
+                class='fa fa-money-check'
+                style={{ fontSize: 50, color: '#128DE2' }}
+              ></i>
+            </div>
+            <div className='fw-6' style={{ margin: '10px 0px' }}>
+              888888 điểm
+            </div>
+          </h4>
+        </div>
+      </div>
+
+      <div
+        class='title'
+        style={{
+          textAlign: 'center',
+          justifyContent: 'space-evenly',
+          marginTop: '20px'
+        }}
+      >
+        <div className='card' style={{ width: '30%' }}>
+          <img
+            style={{
+              width: `70px`,
+              display: `block`,
+              border: `0px solid #707070`,
+              borderRadius: `50%`,
+              padding: `13px`,
+              display: 'block',
+              marginLeft: '32%',
+              height: '64px',
+              backgroundColor: `#f0e89d`
+            }}
+            src='https://cellphones.com.vn/smember/_nuxt/img/gift-box(1)1.ad696df.png'
+          />
+          <h4 style={{ textAlign: 'center' }}>
+            <br />{' '}
+            <span className='fw-7 fs-20' style={{ color: `#128DE2` }}>
+              Ưu đãi của bạn
+            </span>
+
+            <br/>
+
+            <span className='fw-5 fs-16'> 0 ưu đãi </span>
+
+            <br/>
+
+            <Button
+              style={{
+                backgroundColor: `#128DE2`,
+                color: `white`,
+                marginLeft: "5px",
+                marginTop: '10px'
+              }}
+              variant="outlined"
+              startIcon={<i class="fa-regular fa-eye"></i>}
+            >
+              Xem chi tiết
+            </Button>
+
+          </h4>
+          <h4></h4>
+        </div>
+
+        <div className='card' style={{ width: '30%' }}>
+          <img
+            style={{
+              width: `70px`,
+              display: `block`,
+              border: `0px solid #707070`,
+              borderRadius: `50%`,
+              padding: `13px`,
+              display: 'block',
+              marginLeft: '32%',
+              height: '64px',
+              backgroundColor: `#fff7ca`
+            }}
+            src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKj6kzhiUMtjknupRb6nfEdJQZX1itw8JFKQ&usqp=CAU'
+          />
+          <h4 style={{ textAlign: 'center' }}>
+            <br />{' '}
+            <span className='fw-7 fs-20' style={{ color: `#128DE2` }}>
+              Đơn hàng của bạn
+            </span>
+
+            <br/>
+
+            <span className='fw-5 fs-16'> 0 đơn hàng </span>
+
+            <br/>
+
+            <Button
+              style={{
+                backgroundColor: `#128DE2`,
+                color: `white`,
+                marginLeft: "5px",
+                marginTop: '10px'
+              }}
+              variant="outlined"
+              startIcon={<i class="fa-regular fa-eye"></i>}
+            >
+              Xem chi tiết
+            </Button>
+
+          </h4>
+          <h4></h4>
+        </div>
+
+
+        <div className='card' style={{ width: '30%' }}>
+          <img
+            style={{
+              width: `70px`,
+              display: `block`,
+              border: `0px solid #707070`,
+              borderRadius: `50%`,
+              padding: `13px`,
+              display: 'block',
+              marginLeft: '32%',
+              height: '64px',
+              backgroundColor: `#89c1f5`
+            }}
+            src='https://cellphones.com.vn/smember/_nuxt/img/crown.d2de999.png'
+          />
+          <h4 style={{ textAlign: 'center' }}>
+            <br />{' '}
+            <span className='fw-7 fs-20' style={{ color: `#128DE2` }}>
+              Hạng thành viên
+            </span>
+
+            <br/>
+
+            <span className='fw-5 fs-16'> Vô danh tiểu tốt </span>
+
+            <br/>
+
+            <Button
+              style={{
+                backgroundColor: `#128DE2`,
+                color: `white`,
+                marginLeft: "5px",
+                marginTop: '10px'
+              }}
+              variant="outlined"
+              startIcon={<i class="fa-regular fa-eye"></i>}
+            >
+              Xem chi tiết
+            </Button>
+
+          </h4>
+          <h4></h4>
+        </div>
+
+      </div>
+
+      <br />
+    </>
+  )
+}
+export default Orders
--- src/pages/SearchOrderPage/Orders.js
+++ src/pages/SearchOrderPage/Orders.js
@@ -1,20 +1,67 @@
-import React from "react";
-import Button from "@mui/material/Button";
-import "./SearchOrderPage.css";
-import { Divider } from "antd";
+import React from 'react'
+import Button from '@mui/material/Button'
+import './SearchOrderPage.css'
+import { Divider } from 'antd'
 
 const Orders = () => {
   return (
     <>
-      <div class="card">
-        <div class="title">
+      <div style={{ width: `25%`, margin: `0px auto` }}>
+        <div
+          style={{
+            width: `70px`,
+            display: `block`,
+            border: `1px solid #707070`,
+            borderRadius: `50%`,
+            padding: `10px`,
+            display: 'block',
+            marginLeft: '32%',
+            height: '70px',
+            backgroundImage: `url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz0LTGso_7ubZRD1CZoDauy3NWC6RxMqVf_A&usqp=CAU)`,
+            backgroundSize: 'cover'
+          }}
+        >
+          {' '}
+        </div>
+        <h4 style={{ textAlign: 'center' }}>
+          Xin chào
+          <br />{' '}
+          <span className='fw-7 fs-20' style={{ color: `#128DE2` }}>
+            Hoàng Văn Tám
+          </span>
+        </h4>
+        <h4></h4>
+      </div>
+
+      <div class='card title' style={{ justifyContent: 'space-around', marginTop: '10px'}}>
+        <div style={{ textAlign: 'center', width : '45%', color: '#128DE2'}}>
+          <span className='fs-20 fw-8'>0</span>
+          <br/>
+          <span className='fs-20 fw-8'>Đơn hàng</span>
+        </div>
+
+        <div style={{ width: '1px', height: '70px', background: 'black'}}></div>
+
+        <div style={{ textAlign: 'center',  width : '45%', color: '#128DE2'}}>
+          <span className='fs-20 fw-8'>0 đ</span>
+          <br/>
+          <span className='fs-20 fw-8'>Tổng tiền tích luỹ</span>
+        </div>
+
+        <div></div>
+
+      </div>
+
+      <br />
+      <div class='card'>
+        <div class='title'>
           <h4>
-            {" "}
-            <span className="fw-6">Đơn hàng</span>: #7895959
+            {' '}
+            <span className='fw-6'>Đơn hàng</span>: #7895959
           </h4>
           <h4>
-            {" "}
-            <span className="fw-6" style={{ color: `green` }}>
+            {' '}
+            <span className='fw-6' style={{ color: `green` }}>
               Đã nhận được hàng
             </span>
           </h4>
@@ -22,27 +69,27 @@ const Orders = () => {
 
         <Divider></Divider>
 
-        <div class="title">
-          <h4 class="title">
+        <div class='title'>
+          <h4 class='title'>
             <img
               style={{ width: `20%` }}
-              src="https://cdn.tgdd.vn/Products/Images/522/281633/Redmi-Pad-Sliver-thumb-org-200x200.jpg"
+              src='https://cdn.tgdd.vn/Products/Images/522/281633/Redmi-Pad-Sliver-thumb-org-200x200.jpg'
             />
-            <span className="fw-6" style={{ marginLeft: "-120px" }}></span>
+            <span className='fw-6' style={{ marginLeft: '-120px' }}></span>
             Xiaomi Redmi Pad (3+64G) Bạc
           </h4>
-          <h4 className="fs-12">
-            {" "}
-            Tổng tiền: <span className="fw-6 fs-16">4.090.000 đ</span>
+          <h4 className='fs-12'>
+            {' '}
+            Tổng tiền: <span className='fw-6 fs-16'>4.090.000 đ</span>
           </h4>
         </div>
 
-        <div className="title">
+        <div className='title'>
           <div></div>
           <div>
             <Button
               style={{ backgroundColor: `#128DE2`, color: `white` }}
-              variant="outlined"
+              variant='outlined'
             >
               Xem chi tiết
             </Button>
@@ -52,15 +99,15 @@ const Orders = () => {
 
       <br />
 
-      <div class="card">
-        <div class="title">
+      <div class='card'>
+        <div class='title'>
           <h4>
-            {" "}
-            <span className="fw-6">Đơn hàng</span>: #7895959
+            {' '}
+            <span className='fw-6'>Đơn hàng</span>: #7895959
           </h4>
           <h4>
-            {" "}
-            <span className="fw-6" style={{ color: `green` }}>
+            {' '}
+            <span className='fw-6' style={{ color: `green` }}>
               Đã nhận được hàng
             </span>
           </h4>
@@ -68,27 +115,27 @@ const Orders = () => {
 
         <Divider></Divider>
 
-        <div class="title">
-          <h4 class="title">
+        <div class='title'>
+          <h4 class='title'>
             <img
               style={{ width: `20%` }}
-              src="https://cdn.tgdd.vn/Products/Images/522/281633/Redmi-Pad-Sliver-thumb-org-200x200.jpg"
+              src='https://cdn.tgdd.vn/Products/Images/522/281633/Redmi-Pad-Sliver-thumb-org-200x200.jpg'
             />
-            <span className="fw-6" style={{ marginLeft: "-120px" }}></span>
+            <span className='fw-6' style={{ marginLeft: '-120px' }}></span>
             Xiaomi Redmi Pad (3+64G) Bạc
           </h4>
-          <h4 className="fs-12">
-            {" "}
-            Tổng tiền: <span className="fw-6 fs-16">4.090.000 đ</span>
+          <h4 className='fs-12'>
+            {' '}
+            Tổng tiền: <span className='fw-6 fs-16'>4.090.000 đ</span>
           </h4>
         </div>
 
-        <div className="title">
+        <div className='title'>
           <div></div>
           <div>
             <Button
               style={{ backgroundColor: `#128DE2`, color: `white` }}
-              variant="outlined"
+              variant='outlined'
             >
               Xem chi tiết
             </Button>
@@ -96,6 +143,6 @@ const Orders = () => {
         </div>
       </div>
     </>
-  );
-};
-export default Orders;
+  )
+}
+export default Orders
