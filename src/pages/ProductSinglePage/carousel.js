6bd07d9384065b09ae079e23f04e02e10f51bfb8
NPhungDung <dungnpph25823@fpt.edu.vn>
2023-11-29 19:44:22 +0700

    [FIX] fix

--- src/pages/ProductSinglePage/carousel.js
+++ src/pages/ProductSinglePage/carousel.js
@@ -1,52 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import { LoadingOutlined } from '@ant-design/icons'
-import { Spin } from 'antd'
-import './style.css'
+import React, { useEffect, useState } from "react";
+import { LoadingOutlined } from "@ant-design/icons";
+import { Spin } from "antd";
+import "./style.css";
 
-export default function App (props) {
-  const [itemSelected, setItemSelected] = useState(0)
-  const [items, setItems] = useState([])
+export default function App(props) {
+  const [itemSelected, setItemSelected] = useState(0);
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
-    setItems(props.images)
-  })
+    setItems(props.images);
+  });
   const cssNoSelected = () => {
     return {
-      width: '10%',
-      height: '20%',
-      borderRadius: '10px',
-      marginRight: '10px',
-      border: '1px solid #d1d5db'
-    }
-  }
+      width: "10%",
+      height: "20%",
+      borderRadius: "10px",
+      marginRight: "10px",
+      border: "1px solid #d1d5db",
+    };
+  };
 
-  const handleChangeSlide = data => {
-    if (data === 'left') {
-      var value = itemSelected - 1
+  const handleChangeSlide = (data) => {
+    if (data === "left") {
+      var value = itemSelected - 1;
       if (value < 0) {
-        setItemSelected(items.length - 1)
+        setItemSelected(items.length - 1);
       } else {
-        setItemSelected(value)
+        setItemSelected(value);
       }
     } else {
-      var value = itemSelected + 1
+      var value = itemSelected + 1;
       if (value > items.length - 1) {
-        setItemSelected(0)
+        setItemSelected(0);
       } else {
-        setItemSelected(value)
+        setItemSelected(value);
       }
     }
-  }
+  };
 
   const cssSelected = () => {
     return {
-      width: '10%',
-      height: '20%',
-      borderRadius: '10px',
-      marginRight: '10px',
-      border: '1px solid rgb(18, 141, 226)'
-    }
-  }
+      width: "10%",
+      height: "20%",
+      borderRadius: "10px",
+      marginRight: "10px",
+      border: "1px solid rgb(18, 141, 226)",
+    };
+  };
 
   return (
     <div>
@@ -56,7 +56,7 @@ export default function App (props) {
             indicator={
               <LoadingOutlined
                 style={{
-                  fontSize: 24
+                  fontSize: 24,
                 }}
                 spin
               />
@@ -66,40 +66,41 @@ export default function App (props) {
       ) : (
         <>
           <div
-            id='toRight'
+            id="toRight"
             style={{
               backgroundImage: `url(${items[itemSelected].url})`,
-              width: '95%',
-              borderRadius: '10px',
-              border: '1px solid #d1d5db',
-              height: '347px',
-              backgroundRepeat: `no-repeat`
+              width: "95%",
+              borderRadius: "10px",
+              border: "1px solid #d1d5db",
+              height: "347px",
+              backgroundRepeat: `no-repeat`,
+              backgroundPosition: "center",
             }}
           >
             <i
-              class='fa fa-angle-left arrow arrow-left'
-              onClick={() => handleChangeSlide('left')}
+              class="fa fa-angle-left arrow arrow-left"
+              onClick={() => handleChangeSlide("left")}
             ></i>
 
             <i
-              class='fa fa-angle-right arrow arrow-right'
-              onClick={() => handleChangeSlide('right')}
+              class="fa fa-angle-right arrow arrow-right"
+              onClick={() => handleChangeSlide("right")}
             ></i>
           </div>
 
           <div
             style={{
-              display: 'flex',
-              alignItems: 'center',
-              marginTop: '20px',
-              position: 'relative',
-              left: '31%'
+              display: "flex",
+              alignItems: "center",
+              marginTop: "20px",
+              position: "relative",
+              left: "31%",
             }}
           >
             {items.map((item, index) => (
               <img
                 src={item.url}
-                alt=''
+                alt=""
                 onClick={() => setItemSelected(index)}
                 style={index === itemSelected ? cssSelected() : cssNoSelected()}
               />
@@ -108,5 +109,5 @@ export default function App (props) {
         </>
       )}
     </div>
-  )
+  );
 }
